ffed070e49e4d28b9b044fff8d8eeeb8c8265d43
Coltontvb <coltontvb@gmail.com>
2020-12-16 13:57:42 -0600

    update user schema with more detail, and utilize utility folder

--- models/user.js
+++ models/user.js
@@ -1,16 +1,30 @@
+const { validateEmail } = require(`../util/validation`);
 const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
     username: { 
         type: String,
-        required: true,
-        unique: true,
+        required: [true, `A user name is required!`],
+        index: { unique: true },
         trim: true,
         minlength: 3,
         maxlength: 50
     },
-}, {
+    email: {
+        type: String,
+        required: [true, `An email is required!`],
+        index: { unique: true },
+        validate: {
+            validator: (input) => {
+                return validateEmail(input);
+            }
+        },
+        message: props => `${props.value} is not a valid email`
+    },
+    password: { type: String, required: true }
+},
+ {
     timestamps: true,
 });
 
---
+++ util/validation.js
@@ -0,0 +1,6 @@
+function validateEmail(input){
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEx.test(String(input).toLowerCase());
+}
+
+module.exports = { validateEmail };
